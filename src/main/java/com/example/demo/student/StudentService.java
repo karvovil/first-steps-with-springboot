28c5b5cfc00ffae2875ce8337698f1b8419214ed
Ville Karvonen <villekarvonen1@gmail.com>
2023-12-11 14:32:13 +0200

    students name and email can be updated with PUT

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class StudentService {
 
@@ -27,5 +29,33 @@ public class StudentService {
 		}
 		studentRepository.save(student);
 	}
-  
+
+	public void deleteStudent(Long id) {
+		if (!studentRepository.existsById(id)){
+			throw new IllegalStateException("can't delete nonexistent student");
+		}
+		studentRepository.deleteById(id);
+	}
+
+	@Transactional
+	public void updateStudent(Long id, String name, String email) {
+		Student s = studentRepository.findById(id)
+			.orElseThrow(() -> new IllegalStateException(
+				"can't update nonexistent student"));
+
+		
+		if (studentRepository.findStudentByEmail(email).isPresent()){
+			throw new IllegalStateException("email taken");
+		}
+		if(name != null && name.length() > 0){
+			s.setName(name);
+		}
+		if(
+			email != null 
+			&& email.length() > 0
+			&& !studentRepository.findStudentByEmail(email).isPresent()
+		){
+			s.setEmail(email);}
+	}
+
 }
