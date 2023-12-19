dc32e78287f1a84f8ca9e0bbfdd5d519074faa39
Ville Karvonen <villekarvonen1@gmail.com>
2023-12-19 10:43:20 +0200

    User implements UserDetails methods

--- security/src/main/java/com/ville/security/user/User.java
+++ security/src/main/java/com/ville/security/user/User.java
@@ -44,29 +44,35 @@ public class User implements UserDetails{
   public Collection<? extends GrantedAuthority> getAuthorities() {
     return List.of(new SimpleGrantedAuthority(role.name()));
   }
+
   @Override
   public String getUsername() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    return email;
   }
+
   @Override
   public boolean isAccountNonExpired() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+    return true;
   }
+
   @Override
   public boolean isAccountNonLocked() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+    return true;
   }
+
   @Override
   public boolean isCredentialsNonExpired() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+    return true;
+
   }
+
   @Override
   public boolean isEnabled() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+    return true;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
   }
 }
