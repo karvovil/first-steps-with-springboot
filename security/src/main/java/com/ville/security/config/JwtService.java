86cbe7efe5f101f60b0a7f434b75322ad6bb5e67
Ville Karvonen <villekarvonen1@gmail.com>
2023-12-22 12:06:28 +0200

    functionality for extracting claims from a token

--- security/src/main/java/com/ville/security/config/JwtService.java
+++ security/src/main/java/com/ville/security/config/JwtService.java
@@ -1,12 +1,41 @@
 package com.ville.security.config;
 
+import java.security.Key;
+import java.util.function.Function;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
-  public String extractUsername(String jwt) {
-    return null;
+  private static final String SECRET_KEY = "3f5270c71bf32dd13bebd9eb55e45d5e86061b0647aed0461437807877ebe335";
+
+  private Key getSigningKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims extractAllClaims(String token){
+    return Jwts
+      .parserBuilder()
+      .setSigningKey(getSigningKey())
+      .build()
+      .parseClaimsJws(token)
+      .getBody();
   }
 
 }
