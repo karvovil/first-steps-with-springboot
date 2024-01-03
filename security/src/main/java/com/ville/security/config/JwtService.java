6c43df8bb925054e0aa1fe8ddf60ab379f160eb1
Ville Karvonen <villekarvonen1@gmail.com>
2024-01-03 16:36:40 +0200

    methods for generating jwt tokens

--- security/src/main/java/com/ville/security/config/JwtService.java
+++ security/src/main/java/com/ville/security/config/JwtService.java
@@ -1,12 +1,17 @@
 package com.ville.security.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -15,7 +20,7 @@ public class JwtService {
 
   private static final String SECRET_KEY = "3f5270c71bf32dd13bebd9eb55e45d5e86061b0647aed0461437807877ebe335";
 
-  private Key getSigningKey() {
+  private Key getSignInKey() {
     byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
     return Keys.hmacShaKeyFor(keyBytes);
   }
@@ -29,10 +34,26 @@ public class JwtService {
     return claimsResolver.apply(claims);
   }
 
+  public String generateToken ( UserDetails userDetails ) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken( Map<String, Object> extraClaims, UserDetails userDetails) {
+    return Jwts
+    .builder()
+    .setClaims(extraClaims)
+    .setSubject(userDetails.getUsername())
+    .setIssuedAt(new Date(System.currentTimeMillis()))
+    .setExpiration(new Date(System.currentTimeMillis()  + 1000 * 60 * 24))
+    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+    .compact();
+
+  }
+
   private Claims extractAllClaims(String token){
     return Jwts
       .parserBuilder()
-      .setSigningKey(getSigningKey())
+      .setSigningKey(getSignInKey())
       .build()
       .parseClaimsJws(token)
       .getBody();
