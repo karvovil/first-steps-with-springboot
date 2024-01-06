f27b28e875e68cf9e553b5933e573a55646c8578
Ville Karvonen <villekarvonen1@gmail.com>
2024-01-06 15:49:00 +0200

    register User

--- security/src/main/java/com/ville/security/auth/AuthenticationService.java
+++ security/src/main/java/com/ville/security/auth/AuthenticationService.java
@@ -1,6 +1,12 @@
 package com.ville.security.auth;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.ville.security.config.JwtService;
+import com.ville.security.user.Role;
+import com.ville.security.user.User;
+import com.ville.security.user.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,8 +14,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+  private final UserRepository repository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+
   public AuthenticationResponse register(RegisterRequest request) {
-    return null;
+    var user = User.builder()
+      .firstName(request.getFirstName())
+      .lastName(request.getLastName())
+      .email(request.getEmail())
+      .password(passwordEncoder.encode(request.getPassword()))
+      .role(Role.USER)
+      .build();
+
+    repository.save(user);
+
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+      .token(jwtToken)
+      .build();
   }
 
   public AuthenticationResponse authenticate(AuthenticationRequest request) {
